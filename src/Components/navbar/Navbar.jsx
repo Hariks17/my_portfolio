843b8fccc621fd31803091b89ecbcdc8d77fac8c
Hariks17 <hariksh.94@gmail.com>
2024-05-25 23:38:34 +0100

    Changed Header and added social media section

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Navbar.css";
+import { FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaInstagram } from "react-icons/fa";
+
 const Navbar = () => {
   return (
     <section className="hero-header">
@@ -37,6 +41,29 @@ const Navbar = () => {
         <p className="hello_world">{`<>Hello World!</>`}</p>
         <p className="my_name">Im Hariharan Sankar</p>
         <button className="download_cv">Download CV!</button>
+
+        <div className="social_media_container">
+          <div className="group_1">
+            <p className="group_header">Lets Collaborate</p>
+            <ul className="socia-media-list">
+              <li className="social-media-list-element">
+                <FaLinkedin />
+              </li>
+              <li className="social-media-list-element">
+                <SiGmail />
+              </li>
+            </ul>
+          </div>
+          <div className="group_2">
+            <p className="group_header">Send me Reels on</p>
+
+            <ul className="socia-media-list">
+              <li className="social-media-list-element">
+                <FaInstagram />
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
