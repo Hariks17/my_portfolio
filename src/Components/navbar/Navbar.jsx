723052e341749fd0161b930c80d207d5dcfc24dd
Hariks17 <hariksh.94@gmail.com>
2024-05-26 00:20:24 +0100

    Hamburger menu added

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { FaLinkedin } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { FaInstagram } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseLargeLine } from "react-icons/ri";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <section className="hero-header">
       <nav className="nav">
@@ -26,15 +29,40 @@ const Navbar = () => {
                 My Projects
               </a>
             </li>
+          </ul>
+        </div>
+        <div className="contact_me">
+          <button className="contact_me_btn">Contact Me</button>
+        </div>
+        <div
+          className="hamburger-menu"
+          onClick={() => setToggle((val) => !val)}
+        >
+          {toggle ? <RiCloseLargeLine /> : <GiHamburgerMenu />}
+        </div>
+        <div className={toggle ? "mobile_menu active" : "mobile_menu"}>
+          <ul className="menu__list_mobile">
+            <li className="menu__list__item_mobile">
+              <a href="#" className="menu__list__link">
+                About Me
+              </a>
+            </li>
+            <li className="menu__list__item">
+              <a href="#" className="menu__list__link">
+                Tech Stacks
+              </a>
+            </li>
+            <li className="menu__list__item">
+              <a href="#" className="menu__list__link">
+                My Projects
+              </a>
+            </li>
             <li className="menu__list__item">
               <a href="#" className="menu__list__link">
                 Contact Me
               </a>
             </li>
           </ul>
-        </div>
-        <div className="contact_me">
-          <button className="contact_me_btn">Contact Me</button>
         </div>
       </nav>
       <div className="hero-container">
