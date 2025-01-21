02830d54acbb751008552748e2d45c52c1c65884
Hariks17 <hariksh.94@gmail.com>
2025-01-21 13:57:28 +0530

    Tailwind Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-import AboutMe from "./Components/aboutme/AboutMe";
-import Technology from "./Components/technology/Technology";
-import Header from "./Components/header/Header";
-
 const App = () => {
-  return (
-    <main>
-      <Header />
-      <AboutMe />
-      <Technology />
-    </main>
-  );
+  return <div>App</div>;
 };
 
 export default App;
